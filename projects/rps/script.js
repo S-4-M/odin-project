e412c1218afc7a43f3c8c42871b5ee95e202f13f
sam abulafia <sa7email@gmail.com>
2023-09-04 17:12:48 +0300

    change the rock paper scissors script
    add rock paper scissors logic with the shoot function

--- projects/rps/script.js
+++ projects/rps/script.js
@@ -1,11 +1,25 @@
+const options = ["rock", "paper", "scissors"];
+
 const getComputerChoice = () => {
   const randomNum = Math.floor(Math.random() * 3);
-  const options = ["rock", "paper", "scissors"];
   return options[randomNum];
 };
 
 const shoot = (me, com) => {
-  return [me, com];
+  const versus = [me, com];
+  console.log(`me:${me} com:${com}`);
+
+  if (versus.includes(options[0]) && versus.includes(options[2])) {
+    if (me === options[0]) {
+      return "you win";
+    }
+    return "you lose";
+  } else if (options.indexOf(me) > options.indexOf(com)) {
+    return "you win";
+  } else if (options.indexOf(me) < options.indexOf(com)) {
+    return "you lose";
+  }
+  return "drew";
 };
 
 const buttons = document.querySelectorAll(".btn");
